141f293e37ef75145097ad12b70958c05ba613f0
Stefan Fleiter <stefan.fleiter@web.de>
2013-11-24 22:32:57 +0100

    add example project for a bug in cdi-unit

---
+++ cdi-unit-problem/src/test/java/com/github/sfleiter/sandbox/cdi_unit_problem/DummyTest.java
@@ -0,0 +1,17 @@
+package com.github.sfleiter.sandbox.cdi_unit_problem;
+
+import static org.junit.Assert.assertTrue;
+
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(CdiRunner.class)
+public class DummyTest {
+
+    @Test
+    public void test() throws Exception {
+        assertTrue(true);
+    }
+
+}
